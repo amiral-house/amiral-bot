919c00cdd9015fbbdcd25f41e46e1c26563dd3bf
Egor Kolesnikov <freethinkel@gmail.com>
2022-03-24 14:55:33 +0300

    fix: а че

--- index.ts
+++ index.ts
@@ -56,7 +56,7 @@ bot.hears(/^http (\d+)$/gim, (ctx) => {
 });
 
 bot.hears(/^((\s*?)(а че\??)|(а чё\??)(\s*?))+$/gim, (ctx) => {
-  ctx.reply("А ниче, нормально разговаривай", {
+  ctx.reply("А ниче, нормально общайся", {
     reply_to_message_id: ctx.message.message_id,
   });
 });
